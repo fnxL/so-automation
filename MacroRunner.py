66501e134ef0d0a924ae5f55dff5ddf6c1b7cd30
Ajay Kanki <48607132+fnxL@users.noreply.github.com>
2025-05-26 02:19:49 +0530

    update: improve error handling

--- MacroRunner.py
+++ MacroRunner.py
@@ -3,17 +3,6 @@ import threading
 import win32com.client
 from Logger import Logger
 
-
-# pwa_app = pywinauto.application.Application()
-# w_handle = pywinauto.findwindows.find_windows(
-#     title="Microsoft Visual Basic", class_name="#32770"
-# )[0]
-# window = pwa_app.window_(handle=w_handle)
-# window.SetFocus()
-# ctrl = window["&End"]
-# ctrl.Click()
-
-
 class MacroRunner:
     def __init__(self, macro_path: str, macro_name: str, logger: Logger):
         self.macro_path = macro_path
@@ -22,6 +11,8 @@ class MacroRunner:
 
     def run(self):
         self.logger.info(f"Attempting to open Excel and Run Macro: {self.macro_name}")
+        excel = None
+        workbook = None
         try:
             excel = win32com.client.Dispatch("Excel.Application")
             excel.Visible = True
@@ -30,19 +21,21 @@ class MacroRunner:
                 f"Excel started successfully, opening workbook: {self.macro_path}"
             )
             workbook = excel.Workbooks.Open(self.macro_path)
+        except Exception as e:
+            self.logger.error(f"Failed to start Excel/Open workbook: {e}")
+            raise e
 
+        try:
             self.logger.log(f"Running Macro: {self.macro_name}")
-            # Start macro error thread
+            # Create different threads here
             error_thread = threading.Thread(target=self.handle_excel_macro_errors)
             error_thread.start()
 
             excel.Application.Run(self.macro_name)
 
-            error_thread.join(timeout=10)
-
         except Exception as e:
-            self.logger.error(f"Failed to start Excel: {e}")
-            return
+            self.logger.error(f"Error running macro: {e}")
+            raise e
         finally:
             # Clean up
             excel.DisplayAlerts = False
@@ -50,11 +43,29 @@ class MacroRunner:
             excel.Quit()
             self.logger.log("Excel closed successfully.")
 
+            # Join threads here
+            error_thread.join(timeout=10)
+
     def handle_excel_macro_errors(self):
-        app = Application().connect(title_re=".*Excel", class_name="XLMAIN")
-        dlg = app.window(title_re=".*Visual Basic", class_name="#32770")
-        dlg.wait("exists", timeout=50000)
-        error_description = dlg["Static"].texts()[0]
-        dlg["End"].click()
-        self.logger.error(f"Macro Error: {error_description}")
-        raise Exception(f"Macro Error: {error_description}")
+        try:
+            app = Application().connect(title_re=".*Excel", class_name="XLMAIN")
+            dlg = app.window(title_re=".*Visual Basic", class_name="#32770")
+            dlg.wait("exists", timeout=50000)
+            error_description = dlg["Static"].texts()[0]
+            dlg["End"].click()
+            self.logger.error(f"Macro Error: {error_description}")
+            return
+        except Exception as e:
+            self.logger.error(f"Failed to handle Excel macro error: {e}")
+            raise e
+
+    def handle_sap_alert(self):
+        try:
+            app = Application().connect(title_re=".*SAP")
+            alert = app.window(title_re=".*SAP Logon", class_name="#32770")
+            alert.wait("exists", timeout=50000)
+            alert["OK"].click()
+            self.logger.log("SAP Alert handled successfully.")
+        except Exception as e:
+            self.logger.error(f"Failed to handle SAP alert: {e}")
+            raise
