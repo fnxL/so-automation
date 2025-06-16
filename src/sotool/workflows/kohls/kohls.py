3fd20584cc6fa7f500bed3d3a059021d3cfc7415
Ajay Kanki <48607132+fnxL@users.noreply.github.com>
2025-06-16 12:48:47 +0530

    fix: deafault na values in mastersheet

--- src/sotool/workflows/kohls/kohls.py
+++ src/sotool/workflows/kohls/kohls.py
@@ -101,7 +101,7 @@ class Kohls:
 
     def _get_mastersheet_row(self, upc):
         if self.mastersheet_df is None:
-            self.mastersheet_df = get_df_from_excel(path=self._mastersheet_path)
+            self.mastersheet_df = get_df_from_excel(path=self._mastersheet_path, keep_default_na=False, na_values=[])
 
         result = self.mastersheet_df.query(f"upc=={upc}")
         if result.empty:
